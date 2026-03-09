f90b927d528f0ac7a2f1a9ab77dbaaaa1a6a3c42
jemila <abubakarjemiletu08@gmail.com>
2026-03-09 07:01:38 +0000

    fix image import casing for Linux deployment

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,7 +6,7 @@ import image2 from './image/image2.png'
 import image5 from './image/image5.png'
 import image19 from './image/image19.jpg'
 
-import image23 from './image/image23.JPEG'
+import image23 from './image/image23.jpeg'
 import image27 from './image/image27.JPEG'
 import image36 from './image/image36.JPG'
 
